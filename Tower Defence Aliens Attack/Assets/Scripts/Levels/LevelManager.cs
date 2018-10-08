3e578ab0f27a0a62a8104f898ee0b377451dd761
Serhii Alkovych <serhii.alkovych@outlook.com>
2018-10-08 23:09:39 +0200

    Level and Scene loading bar
    Started development of the first level of the game. Added the creation of field maps. Added scene loading bar demo version

---
+++ Tower Defence Aliens Attack/Assets/Scripts/Levels/LevelManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LevelManager : MonoBehaviour
+
+    // Данный клас создает поле битвы для игры 
+
+{
+    [SerializeField] // для сокрытия данных , но по прежнему доступно в редакторе
+    private GameObject[] tilePrefabs; //замля которая идет по координатам х и y 
+
+    // подсчет на сколько большие кусочки нашего пола для компоновки их вместе без стыков // Свойство
+    public float TileSize
+    {
+       get { return tilePrefabs[0].GetComponent<SpriteRenderer>().sprite.bounds.size.x; }
+    }
+
+    void Start()
+    {
+        CreateLevel();
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    private void CreateLevel() // Данная функция создает уровень , нарисовка карты с помощью квадратов.
+    {
+
+        string[] mapData = ReadLevelText();
+
+
+        int mapXSize = mapData[0].ToCharArray().Length;
+        int mapYSize = mapData.Length;
+
+        // подсчет на сколько большие кусочки нашего пола для компоновки их вместе без стыков
+        //float tileSize = tile.GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+
+        Vector3 worldStart = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height)); // для нахождения левого верхнего угла для правильного построения карты 
+
+        for (int y = 0; y < mapYSize; y++)
+        {
+
+            char[] newTiles = mapData[y].ToCharArray();
+
+            for (int x = 0; x < mapXSize; x++)
+            {
+                PlaceTile(newTiles[x].ToString(), x,y, worldStart);
+            }
+        }
+    }
+
+    //Размещает кусочки земли
+    private void PlaceTile(string tileType, int x, int y , Vector3 willStart)
+    {
+        int tileIndex = int.Parse(tileType);
+
+        GameObject newTile = Instantiate(tilePrefabs[tileIndex]);
+        newTile.transform.position = new Vector3(willStart.x + (TileSize * x), willStart.y - (TileSize * y), 0); // разметка полей , на данном цыкле 25 квадратов вместе.
+    }
+
+
+    private string[] ReadLevelText()
+    {
+        TextAsset bindData = Resources.Load("Level") as TextAsset;//загрузка карты с файла
+
+        string data = bindData.text.Replace(Environment.NewLine, string.Empty);
+
+        return data.Split('-'); 
+    }
+}
---
+++ Tower Defence Aliens Attack/Assets/Scripts/MainMenu/SceneLoading.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoading : MonoBehaviour {
+
+    [Header("Loading Scenes")]
+    public int sceneID;
+
+    [Header("Other objects")]
+    public Image loadingImage;
+    public Text progressText;
+
+    void Start () {
+        StartCoroutine(AsyncLoad());
+	}
+     
+
+    IEnumerator AsyncLoad()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID); // загружаем сцену используя ID сцены .. Для многоразоваго использования.
+        while(!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            loadingImage.fillAmount = progress;
+            progressText.text = string.Format("{0:0}%",progress*100);
+            yield return null;
+        }
+
+    }
+	
+}
